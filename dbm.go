17a48c1954880e45bc7490ac4d66e56e41a9e58a
Michel Casabianca <casa@sweetohm.net>
2016-04-10 00:07:00 +0200

    Added configuration

--- dbm.go
+++ dbm.go
@@ -2,10 +2,51 @@ package main
 
 import (
 	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"strconv"
 	"strings"
 )
 
+// configuration
+type DatabaseConfiguration struct {
+	Hostname string
+	Database string
+	Username string
+	Password string
+}
+
+type Configuration struct {
+	Platforms         []string
+	DefaultPlatform   string   `yaml:"default-platform"`
+	CriticalPlatforms []string `yaml:"critical-platforms"`
+	SqlDir            string   `yaml:"sql-dir"`
+	DatabaseCharset   string   `yaml:"database-charset"`
+	Database          map[string]DatabaseConfiguration
+}
+
+func ParseConfiguration(source []byte) (*Configuration, error) {
+	var configuration Configuration
+	err := yaml.Unmarshal(source, &configuration)
+	if err != nil {
+		return nil, err
+	}
+	return &configuration, nil
+}
+
+func LoadConfiguration(file string) (*Configuration, error) {
+	source, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	configuration, err := ParseConfiguration(source)
+	if err != nil {
+		return nil, err
+	}
+	return configuration, nil
+}
+
+// version as an array of integers
 type Version []int
 
 func NewVersion(v string) (Version, error) {
@@ -32,4 +73,30 @@ func (v1 Version) CompareTo(v2 Version) int {
 		}
 	}
 	return len(v1) - len(v2)
+}
+
+func (v Version) String() string {
+	s := ""
+	for _, i := range v {
+		if s != "" {
+			s += "."
+		}
+		s += strconv.Itoa(i)
+	}
+	return s
+}
+
+// List of versions for sorting
+type Versions []Version
+
+func (v Versions) Len() int {
+	return len(v)
+}
+
+func (v Versions) Swap(i, j int) {
+	v[i], v[j] = v[j], v[i]
+}
+
+func (v Versions) Less(i, j int) bool {
+	return v[i].CompareTo(v[j]) < 0
 }
--- dbm_test.go
+++ dbm_test.go
@@ -1,6 +1,61 @@
 package main
 
-import "testing"
+import (
+	"sort"
+	"testing"
+)
+
+func TestParseConfiguration(t *testing.T) {
+	source := `# platform list
+platforms:
+- itg
+- prp
+- prod
+# default platform
+default-platform: itg
+# platform where init is forbidden
+critical-platforms: [prod]
+# directory for SQL scripts (relative to this file)
+sql-dir: sql
+# charset of the database
+database-charset: utf8
+# Database configuration for environments
+database:
+  itg:
+    hostname: localhost
+    database: test
+    password: test
+    username: test
+  prp:
+    hostname: localhost
+    database: test
+    password: test
+    username: test
+  prod:
+    hostname: localhost
+    database: test
+    password: test
+    username: test`
+	configuration, err := ParseConfiguration([]byte(source))
+	if err != nil {
+		t.Fatalf("Error parsing configuration: %s", err)
+	}
+	if configuration.Platforms[0] != "itg" ||
+		configuration.Platforms[1] != "prp" ||
+		configuration.Platforms[2] != "prod" ||
+		configuration.DefaultPlatform != "itg" ||
+		len(configuration.CriticalPlatforms) != 1 ||
+		configuration.CriticalPlatforms[0] != "prod" ||
+		configuration.SqlDir != "sql" ||
+		configuration.DatabaseCharset != "utf8" ||
+		len(configuration.Database) != 3 ||
+		configuration.Database["itg"].Hostname != "localhost" ||
+		configuration.Database["itg"].Database != "test" ||
+		configuration.Database["itg"].Username != "test" ||
+		configuration.Database["itg"].Password != "test" {
+		t.Fatal("Loaded bad configuration")
+	}
+}
 
 func TestNewVersion(t *testing.T) {
 	v, err := NewVersion("1.2.3")
@@ -65,6 +120,33 @@ func TestVersionCompareTo(t *testing.T) {
 	}
 	v2, _ = NewVersion("1.2.3")
 	if v1.CompareTo(v2) != 0 {
+		t.Fail()
+	}
+}
+
+func TestVersionString(t *testing.T) {
+	s := "1.2.3"
+	v, _ := NewVersion(s)
+	if v.String() != s {
+		t.Fail()
+	}
+}
+
+func TestSortVersions(t *testing.T) {
+	v1, _ := NewVersion("1.2.3")
+	v2, _ := NewVersion("1.2")
+	v3, _ := NewVersion("1")
+	v4, _ := NewVersion("1.2.4")
+	v5, _ := NewVersion("2.2")
+	v6, _ := NewVersion("0.1")
+	v := []Version{v1, v2, v3, v4, v5, v6}
+	sort.Sort(Versions(v))
+	if v[0].CompareTo(v6) != 0 &&
+		v[1].CompareTo(v3) != 0 &&
+		v[2].CompareTo(v2) != 0 &&
+		v[3].CompareTo(v1) != 0 &&
+		v[4].CompareTo(v4) != 0 &&
+		v[5].CompareTo(v5) != 0 {
 		t.Fail()
 	}
 }
