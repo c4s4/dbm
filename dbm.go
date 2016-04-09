57152ce53a7a448354595034da30ed11c013d165
Michel Casabianca <casa@sweetohm.net>
2016-04-09 19:59:52 +0200

    Implemented Version

--- dbm.go
+++ dbm.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
-    println("Hello World!")
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Version []int
+
+func NewVersion(v string) (Version, error) {
+	version := []int{}
+	for _, s := range strings.Split(v, ".") {
+		i, err := strconv.Atoi(s)
+		if err != nil {
+			return nil, fmt.Errorf("Error parsing version '%s'", v)
+		}
+		version = append(version, i)
+	}
+	return version, nil
+}
+
+func (v1 Version) CompareTo(v2 Version) int {
+	l := len(v1)
+	if len(v2) < l {
+		l = len(v2)
+	}
+	for i := 0; i < l; i++ {
+		d := v1[i] - v2[i]
+		if d != 0 {
+			return d
+		}
+	}
+	return len(v1) - len(v2)
 }
